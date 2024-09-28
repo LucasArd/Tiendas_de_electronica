9928ff80b9c2c51679322fbe050336d7eac4dd34
LucasArd <luqasaranda@outlook.com>
2024-09-28 12:29:24 -0300

    Inicio proyecto

---
+++ src/main/java/com/proyectoposnet/tiendas_de_electronica/Tiendas_de_electronica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyectoposnet.tiendas_de_electronica;
+
+/**
+ *
+ * @author luqas
+ */
+public class Tiendas_de_electronica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
