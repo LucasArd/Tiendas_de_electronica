18c0082e50d49ef70468b0a478bf720bdae291c6
LucasArd <luqasaranda@outlook.com>
2024-09-28 18:02:57 -0300

    TiendasElectronicas

--- src/main/java/com/proyectoposnet/tiendas_de_electronica/Tiendas_de_electronica.java
+++ src/main/java/com/proyectoposnet/tiendas_de_electronica/Tiendas_de_electronica.java
@@ -1,16 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.proyectoposnet.tiendas_de_electronica;
 
-/**
- *
- * @author luqas
- */
 public class Tiendas_de_electronica {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Empresa empresa = new Empresa();
+        cargarEmpresa(empresa);
     }
+    
+    private static void cargarEmpresa(Empresa empresa){
+        Sucursal suc1 = new Sucursal("Sucursal A");
+        Sucursal suc2 = new Sucursal("Sucursal B");
+        
+        suc1.agregarDispositivo(new Dispositivo("AA123", 12400, TipoDispositivo.COMPUTADORA));
+        suc1.agregarDispositivo(new Dispositivo("JF323", 10000, TipoDispositivo.TELEFONO));
+        
+        suc2.agregarDispositivo(new Dispositivo("AJJ23", 11200, TipoDispositivo.TABLET));
+        suc2.agregarDispositivo(new Dispositivo("LLA123", 15400, TipoDispositivo.TABLET));
+        suc2.agregarDispositivo(new Dispositivo("BGD2223", 20400, TipoDispositivo.TELEFONO));
+        
+        empresa.agregarSucursal(suc1);
+        empresa.agregarSucursal(suc2);
+        
+        empresa.listarDispositivos();
+        
+        for(Dispositivo d: empresa.dispositivosPorTipo(TipoDispositivo.TELEFONO)){
+            System.out.println(d);
+        }
+        
+    }
+    
 }
